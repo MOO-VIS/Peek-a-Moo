069ff8497de1ec722d1ee5a6f48edb1ab5bd4a22
datallurgy <94504640+datallurgy@users.noreply.github.com>
2022-05-26 15:30:10 -0700

    add elo data load

--- dashboard/global.R
+++ dashboard/global.R
@@ -74,6 +74,7 @@ if (!exists("THI")) {
   load("../data/Feeding_drinking_at_the_same_time_total_time.Rda")
   load("../data/Feeding_drinking_neighbour_total.Rda")
   load("../data/Replacement_behaviour_by_date.Rda")
+  load("../data/_10-mon__elo_all_replacements_long_noNA.rda")
   
   THI <- master_summary
   
