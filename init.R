f694387ee149c33943390f01a409a77c5b0a24c9
datallurgy <allyson@datallurgy.com>
2022-06-21 12:59:21 -0700

    comment out report generation

--- init.R
+++ init.R
@@ -1,5 +1,7 @@
 helpers.installPackages('shinydashboard', 'shinyWidgets', 'shinymanager', 
     'Rcpp', 'dplyr', 'lubridate', 'plotly', 'igraph', 'visNetwork',
     'shinycssloaders', 'tidyr', 'tibble', 'purrr', 'forcats', 'stringr', 
-    'shinyBS', 'shinyalert', 'rmarkdown', 'rstan', 'rstantools','StanHeaders',
-    'akima', 'DBI', 'RPostgres','dbplyr', 'stats','graphics','ggplot2')
+    'shinyBS', 'shinyalert', 'DBI', 'RPostgres','dbplyr', 'ggplot2')
+    
+# helpers.installPackages('rmarkdown', 'rstan', 'rstantools','StanHeaders',
+#     'akima', 'stats', 'graphics')
