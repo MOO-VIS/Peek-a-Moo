3544878f94cc68abb4e5f783a2ba226042eea9e5
datallurgy <94504640+datallurgy@users.noreply.github.com>
2022-06-21 16:42:37 -0700

    Update init.R

--- init.R
+++ init.R
@@ -1,7 +1,7 @@
 helpers.installPackages('shinydashboard', 'shinyWidgets', 'shinymanager', 
     'Rcpp', 'dplyr', 'lubridate', 'plotly', 'igraph', 'visNetwork',
     'shinycssloaders', 'tidyr', 'tibble', 'purrr', 'forcats', 'stringr', 
-    'shinyBS', 'shinyalert', 'DBI', 'RPostgres','dbplyr', 'ggplot2')
+    'shinyBS', 'shinyalert', 'DBI', 'RPostgres','dbplyr', 'ggplot2', 'here')
     
 # helpers.installPackages('rmarkdown', 'rstan', 'rstantools','StanHeaders',
 #     'akima', 'stats', 'graphics')
